3df444122a907a1c637ae4ddb5f13c4f35f4d810
hanye-lun <3055397738@qq.com>
2022-04-22 22:53:23 +0800

    重命名模块为udr

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 mod op;
 
+use crate::op::directory::DirectoryItem;
+use crate::op::file::File;
+use reqwest::Client;
 use std::borrow::Borrow;
 use std::hash::Hasher;
 use std::sync::{Arc, Mutex};
-use reqwest::Client;
 use tokio::fs::{create_dir_all, OpenOptions};
 use tokio::net::windows::named_pipe::PipeEnd::Client;
-use crate::op::directory::DirectoryItem;
-use crate::op::file::File;
 
 #[tokio::main]
 async fn main() {
@@ -24,21 +24,16 @@ async fn main() {
     }
 }
 
-async fn query_project(
-    project_name: &str
-) -> Result<DirectoryItem, Box<dyn std::error::Error>>
-{
-    let body: String = reqwest::get(format!("https://unpkg.com/{}/?meta", project_name)).await?
-        .text().await?;
+async fn query_project(project_name: &str) -> Result<DirectoryItem, Box<dyn std::error::Error>> {
+    let body: String = reqwest::get(format!("https://unpkg.com/{}/?meta", project_name))
+        .await?
+        .text()
+        .await?;
     let project: DirectoryItem = serde_json::from_str(&body)?;
     return Ok(project);
 }
 
-async fn download_file(
-    project_name: &str,
-    files: Vec<&File>
-)
-{
+async fn download_file(project_name: &str, files: Vec<&File>) {
     let files_iter = Arc::new(Mutex::new(files.iter()));
     for i in 0..5 {
         async {
@@ -47,10 +42,11 @@ async fn download_file(
             loop {
                 let mut iter = iter_arc.lock().unwrap();
                 if let Some(&&file) = iter.next() {
-                    let req = client.get(format!("https://unpkg.com/{}/{}",project_name,&file.path))
-                        .header("contentType",&file.content_type)
-                        .send().await?;
-
+                    let req = client
+                        .get(format!("https://unpkg.com/{}/{}", project_name, &file.path))
+                        .header("contentType", &file.content_type)
+                        .send()
+                        .await?;
                 } else {
                     break;
                 }
@@ -59,12 +55,12 @@ async fn download_file(
     }
 }
 
-async fn file_write(data:String){
+async fn file_write(data: String) {
     let mut file_operation = OpenOptions::new();
     file_operation.create(true).write(true);
     //新建文件夹
-    create_dir_all(format!("{}/{}",this_clone.2,file_i.0)).unwrap();
+    create_dir_all(format!("{}/{}", this_clone.2, file_i.0)).unwrap();
     //新建文件并写入
-    file_operation.open(format!("{}/{}",this_clone.2,url)).w;
-    println!("{}{} -> {}/{}",this_clone.1,url,this_clone.2,url);
+    file_operation.open(format!("{}/{}", this_clone.2, url)).w;
+    println!("{}{} -> {}/{}", this_clone.1, url, this_clone.2, url);
 }
